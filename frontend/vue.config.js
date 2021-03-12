5a088b2fa07d170709ef4291ca32ed386a51e430
YJSNPIDISK <56146987+YJSNPIDISK@users.noreply.github.com>
2021-03-12 21:15:40 +0900

    fix: Connection Timed Out

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,11 @@
 /* eslint-disable */
 
 module.exports = {
-    configureWebpack: {
-        devServer: {
-            disableHostCheck: true,
-            proxy: {
-                '/api': {
-                    target: 'http://sc0nep.iptime.org:7000'
-                }
+    devServer: {
+        disableHostCheck: true,
+        proxy: {
+            '/api': {
+                target: 'http://sc0nep.iptime.org:7000'
             }
         }
     }
