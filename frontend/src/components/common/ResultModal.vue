1ac8ab0570bb35c9755cf42df06f456efa26343e
YJSNPIDISK <56146987+YJSNPIDISK@users.noreply.github.com>
2021-05-22 21:55:34 +0900

    fix: variable map level

--- frontend/src/components/common/ResultModal.vue
+++ frontend/src/components/common/ResultModal.vue
@@ -81,7 +81,7 @@
         var mapContainer = document.getElementById("book-aladin-map"); // 지도를 표시할 div
         var mapOption = {
           center: new kakao.maps.LatLng(35.77114136986587, 127.84584124824799), // 지도의 중심좌표
-          level: 13, // 지도의 확대 레벨
+          level: document.documentElement.clientWidth>768?13:15, // 지도의 확대 레벨
         };
         this.map = new kakao.maps.Map(mapContainer, mapOption);
         this.location = new kakao.maps.services.Places();
